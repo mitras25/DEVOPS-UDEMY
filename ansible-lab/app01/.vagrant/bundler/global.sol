87ecc76cd92107b8ca8b7747557f20bd8b865575
roberto <mitras25jpn@hotmail.com>
2023-07-02 19:36:05 -0300

    app01

---
+++ ansible-lab/app01/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]],["vagrant-winnfsd",["= 1.4.0"]]],"checksum":"0579a8509f6f61eedbadad9741eb03f50041dbb17da920e024556bdd31b62a1d","vagrant_version":"2.3.7"}
